85489343b6d3e88ab8ffac990d3a8800c283fc17
Black20202020 <119349237+Black20202020@users.noreply.github.com>
2023-01-08 15:15:47 +0300

    Update token.py

--- plugins/token.py
+++ plugins/token.py
@@ -8,8 +8,8 @@ from helpers import parent_id_sql as sql
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-G_DRIVE_CLIENT_ID = "197036948433-4sjgjrj1osm5b5neu8khh7c2nsvn96f7.apps.googleusercontent.com"
-G_DRIVE_CLIENT_SECRET = "dnXoMIu2V7HQ8G8RicrKmvlu"
+G_DRIVE_CLIENT_ID = "774516799175-lcmect6tjkdvkjq5ho2p5inbcj7iflsh.apps.googleusercontent.com"
+G_DRIVE_CLIENT_SECRET = "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"
 flow = None
 
 @Client.on_message(Filters.private & Filters.incoming & Filters.command(['auth']))
